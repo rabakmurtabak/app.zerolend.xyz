29d2b4ebd0e5e58d9d939c9c2775d155c2680b54
ZeroLend <hello@zerolend.xyz>
2023-08-15 14:55:03 +0300

    update img url

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -82,7 +82,7 @@ export default function MyApp(props: MyAppProps) {
         description={
           'ZeroLend is an Open Source Protocol to create Non-Custodial Liquidity Markets to earn interest on supplying and borrowing assets with a variable or stable interest rate. The protocol is designed for easy integration into your products and services.'
         }
-        imageUrl="https://app.aave.com/aaveMetaLogo-min.jpg"
+        imageUrl="https://i.imgur.com/Ee63x7K.png"
       />
       <LanguageProvider>
         <QueryClientProvider client={queryClient}>
